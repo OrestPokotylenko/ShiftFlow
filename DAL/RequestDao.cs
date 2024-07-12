22d192a0fb22630c23d3ae620c09186ab45c2d48
OrestPokotylenko <opokotylenkowork@gmail.com>
2024-07-12 15:58:20 +0200

    request functionality added

---
+++ DAL/RequestDao.cs
@@ -0,0 +1,12 @@
+﻿namespace DAL
+{
+    public class RequestDao
+    {
+        private ShiftFlowContext _context = new();
+
+        public int CountRequests(bool? approve, int employeeId)
+        {
+            return _context.Requests.Count(r => r.Approved == approve && employeeId == r.EmployeeId);
+        }
+    }
+}
