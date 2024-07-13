fc0668e22e9d9336f269b1f5613adf46b7a5bcc1
OrestPokotylenko <opokotylenkowork@gmail.com>
2024-07-13 23:25:52 +0200

    design for profile view added

--- UI/SecondaryViews/ProfileView.xaml.cs
+++ UI/SecondaryViews/ProfileView.xaml.cs
@@ -1,28 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace UI.SecondaryViews
 {
-    /// <summary>
-    /// Interaction logic for ProfileView.xaml
-    /// </summary>
     public partial class ProfileView : UserControl
     {
         public ProfileView()
         {
             InitializeComponent();
+        }
+
+        private void DropDown_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            DockPanel dockPanel = (DockPanel)button.Tag;
+
+            if (dockPanel.Visibility == Visibility.Collapsed)
+            {
+                Storyboard showStoryboard = (Storyboard)Application.Current.Resources["ShowDockPanelStoryboard"];
+                showStoryboard.Begin(dockPanel);
+            }
+            else
+            {
+                Storyboard hideStoryboard = (Storyboard)Application.Current.Resources["HideDockPanelStoryboard"];
+                hideStoryboard.Begin(dockPanel);
+            }
         }
     }
 }
