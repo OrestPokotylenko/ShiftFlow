b88580cd63532ff52777ee2ccab036d5dfdee6f0
OrestPokotylenko <opokotylenkowork@gmail.com>
2024-07-17 19:44:41 +0200

    settings-view design added

--- UI/SecondaryViews/SettingsView.xaml.cs
+++ UI/SecondaryViews/SettingsView.xaml.cs
@@ -1,28 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media.Animation;
+using ViewModel;
 
 namespace UI.SecondaryViews
 {
-    /// <summary>
-    /// Interaction logic for SettingsView.xaml
-    /// </summary>
     public partial class SettingsView : UserControl
     {
         public SettingsView()
         {
             InitializeComponent();
+        }
+
+        private void DropDown_Click(object sender, EventArgs e)
+        {
+            ToggleButton button = (ToggleButton)sender;
+            DockPanel dockPanel = (DockPanel)button.Tag;
+
+            if (dockPanel.Visibility == Visibility.Collapsed)
+            {
+                Storyboard showStoryboard = (Storyboard)Application.Current.Resources["ShowDockPanelStoryboard"];
+                showStoryboard.Begin(dockPanel);
+            }
+            else
+            {
+                Storyboard hideStoryboard = (Storyboard)Application.Current.Resources["HideDockPanelStoryboard"];
+                hideStoryboard.Begin(dockPanel);
+            }
         }
     }
 }
--- ViewModel/SettingsVM.cs
+++ ViewModel/SettingsVM.cs
@@ -2,5 +2,18 @@
 {
     public class SettingsVM : BaseVM
     {
+        private bool _emailNotifications;
+        public bool EmailNotifications
+        {
+            get { return _emailNotifications; }
+            set { _emailNotifications = value; OnPropertyChanged(); }
+        }
+
+        private bool _pushNotifications;
+        public bool PushNotifications
+        {
+            get { return _pushNotifications; }
+            set { _pushNotifications = value; OnPropertyChanged(); }
+        }
     }
 }
